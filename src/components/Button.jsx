3deb02a6e42387ef0116687fa5db2102fce99b59
VastoLorde <ksenbonsakura@gmail.com>
2024-01-25 16:20:56 +0100

    UPDATE: Subscribe section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,8 @@
-const Button = ({ label, iconURL, backgroundColor, borderColor, textColor }) => {
+const Button = ({ label, iconURL, backgroundColor, borderColor, textColor, fullWidth }) => {
   return (
     <button className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none rounded-full
-        ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : `bg-coral-red text-white border-coral-red`}`}
+        ${backgroundColor ? `${backgroundColor} ${borderColor} ${textColor}` : `bg-coral-red text-white border-coral-red`}
+        ${fullWidth && 'w-full'}`}
     >
         {label}
 
